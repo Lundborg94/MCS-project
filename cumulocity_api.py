39def5f7002d1e40438d771c639cf6bb6b5d296f
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2021-02-26 14:57:39 +0100

    implement cumulocity

---
+++ cumulocity_api.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def get_configurations():
+    with open("cumulocity_config.json", "r") as file:
+        config = file.read()
+
+    return json.loads(config)
+
+
+def get_location():
+    config = get_configurations()
+
+    username = config['username']
+    password = config['password']
+
+    payload = { 'fragmentType': 'c8y_Position' }
+    r = requests.get('https://{}.eu-latest.cumulocity.com/inventory/managedObjects'.format(username.lower()), auth=(username, password), params=payload)
+
+    response = r.json()
+
+    managedObjects = response['managedObjects'][0]
+    position = managedObjects['c8y_Position']
+
+    return position
