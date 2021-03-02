4e447ad74feac06fda1a8a623f0334524e9f06a6
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2021-03-02 11:55:52 +0100

    inject tenant_id to request

--- cumulocity_api.py
+++ cumulocity_api.py
@@ -12,14 +12,15 @@ def get_configurations():
 def get_location():
     config = get_configurations()
 
+    tenant_id = config['tenant_id']
     username = config['username']
     password = config['password']
 
     payload = {'fragmentType': 'c8y_Position'}
-    r = requests.get('https://{}.eu-latest.cumulocity.com/inventory/managedObjects'.format(username.lower()),
+    resp = requests.get('https://{}.eu-latest.cumulocity.com/inventory/managedObjects'.format(tenant_id),
                      auth=(username, password), params=payload)
 
-    response = r.json()
+    response = resp.json()
 
     managedObjects = response['managedObjects'][0]
     position = managedObjects['c8y_Position']
