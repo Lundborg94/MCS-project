65878967c7f4c12d3e98670f39ae517146d5d041
Jessica Feder <jessica.feder@student.kyh.se>
2021-03-03 23:52:57 +0100

    Registration page
    SQlite not working

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask, Response, render_template, redirect, url_for, request
 from flask_login import LoginManager, UserMixin, login_required
 from mcs_services import LoginService
 from mcs_repositories import DeviceRepositoryTest
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+import re
 
 app = Flask(__name__)
 login_manager = LoginManager()
@@ -39,7 +41,7 @@ def load_user(request):
                 return user
     return None
 
-
+@app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -61,9 +63,39 @@ def login():
     return render_template('login.html', error=error)
 
 
+@app.route('/register', methods =['GET', 'POST']) 
+def register(): 
+    msg = '' 
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'mail' in request.form and 'vehicle' in request.form and 'deviceid' in request.form :
+        deviceid = request.form['deviceid'] 
+        name = request.form['name'] 
+        password = request.form['password'] 
+        mail = request.form['mail'] 
+        vehicle = request.form['vehicle']
+        cursor = sqlite3.connect.cursor(sqlite3.cursors.DictCursor) 
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (name, )) 
+        account = cursor.fetchone() 
+        if account: 
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', mail): 
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', name): 
+            msg = 'Username must contain only characters and numbers !'
+        elif not name or not password or not mail or not deviceid or not vehicle: 
+            msg = 'Please fill out the form !'
+        else: 
+            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (name, password, mail, vehicle, deviceid )) 
+            sqlite3.connect.commit() 
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST': 
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg) 
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 
 if __name__ == '__main__':
