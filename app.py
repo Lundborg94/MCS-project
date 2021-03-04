73f85599acc60bd330b0096204a071856327becf
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2021-03-04 17:27:33 +0100

    implemented login sessions and remove obsolete code

--- app.py
+++ app.py
@@ -1,58 +1,33 @@
 import sqlite3
+import json
 
-from flask import Flask, Response, render_template, redirect, url_for, request
-from flask_login import LoginManager, UserMixin, login_required
+from flask import Flask, render_template, redirect, url_for, request, session
 from mcs_services import LoginService
 from mcs_repositories import DeviceRepositoryTest
 
 app = Flask(__name__)
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-
-class User(UserMixin):
-    # proxy for a database of users
-    user_database = {"JohnDoe": ("JohnDoe", "John"),
-                     "JaneDoe": ("JaneDoe", "Jane")}
-
-    def __init__(self, deviceID, password):
-        self.id = deviceID
-        self.password = password
-
-    @classmethod
-    def get(cls, id):
-        return cls.user_database.get(id)
-
-
-@login_manager.request_loader
-def load_user(request):
-    token = request.headers.get('Authorization')
-    if token is None:
-        token = request.args.get('token')
-
-    if token is not None:
-        deviceID, password = token.split(":")  # naive token
-        user_entry = User.get(deviceID)
-        if user_entry is not None:
-            user = User(user_entry[0], user_entry[1])
-            if user.password == password:
-                return user
-    return None
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-    if request.method == 'POST':
+
+    if request.method == 'GET' and is_user_in_session():
+        # No login required
+        return redirect(url_for('home'))
+
+    elif request.method == 'POST':
         device_id = request.form['deviceID']
         req_password = request.form['password']
 
         with sqlite3.connect('deviceservice.db') as context:
-            repository = DeviceRepositoryTest(context)
-            service = LoginService(repository)
+            service = LoginService(DeviceRepositoryTest(context))
+            correct_password = service.get_user_password(device_id)
 
-            if req_password == service.get_user_password(device_id):
-                user = service.get_user(device_id)  # TODO: Pass the user data onto the view
+            if req_password == correct_password:
+                user = service.get_user(device_id)
+
+                add_user_to_session(user)
 
                 return redirect(url_for('home'))
             else:
@@ -63,7 +38,26 @@ def login():
 
 @app.route('/')
 def home():
+    if not is_user_in_session():
+        return redirect(url_for('login'))
+
+    print("USER IN SESSION")
+    print("===============")
+    print(json.dumps(session["user"], indent=4))
     return render_template('home.html')
+
+
+def is_user_in_session():
+    return 'user' in session
+
+
+def add_user_to_session(user):
+    session['user'] = {
+        'device': {
+            'device_id': user.device.id,
+            'device_name': user.device.name
+        }
+    }
 
 
 if __name__ == '__main__':
